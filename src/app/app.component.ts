4eee8dbdaaa0d952980071f320c55d49ba85d962
Marty Mazurik <marty@martymazurik.com>
2025-05-21 12:31:36 -0700

    updates for mobile responsiveness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'NG10E2E Reference Application';
+  title = 'Foods App';
+  constructor(public auth: AuthService) {}
 }
